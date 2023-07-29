1800ac43177b072f2545f54651060caf4c19fbac
blood <zhoujie.li@hotmail.com>
2023-07-30 01:38:13 +0200

    register site

--- pages/register.php
+++ pages/register.php
@@ -1,3 +1,19 @@
+<?php
+
+$con = mysqli_connect("localhost", "root", "", "social");
+
+// überprüft ob error hat
+if (mysqli_connect_errno()) {
+    echo "Failed to connect: " . mysqli_connect_errno();
+}
+
+// mit \$con verbindet man der Datenbank 
+$query = mysqli_query($con, "insert into test values (null,'Stjepan');");
+?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,9 +24,20 @@
 </head>
 
 <body>
-    <form action="">
+    <form action="register.php" method="POST">
         <input type="text" name="reg_fname" placeholder="First Name" required>
-        <input type="text" name="reg_lname" placeholder="First Name" required>
+        <br>
+        <input type="text" name="reg_lname" placeholder="Last Name" required>
+        <br>
+        <input type="email" name="reg_email" placeholder="Email" required>
+        <br>
+        <input type="email" name="reg_email2" placeholder="Confirm Email" required>
+        <br>
+        <input type="password" name="reg_password" placeholder="Password" required>
+        <br>
+        <input type="password" name="reg_password2" placeholder="Confirm Password" required>
+
+        <input type="submit" name="register_button" value="Register">
     </form>
 
 
